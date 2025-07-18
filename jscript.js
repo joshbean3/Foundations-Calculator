faa47cd1cc1051684c569e2e82b041dd0dcad6d3
joshbean3 <joshdbean3@gmail.com>
2025-07-17 21:06:41 -0400

    Create operator functions

--- jscript.js
+++ jscript.js
@@ -13,3 +13,32 @@ function multiply(a,b) {
 function divide(a, b) {
     return a / b;
 }
+
+let number1 = 0;
+let operate = "";
+let number2 = 0;
+
+// Create operator function to call operator
+function operate(operator, number1, number2) {
+    if (operator === "+") {
+        return add(number1, number2);
+    } else if (operator === "-") {
+        return subtract(number1, number2);
+    } else if (operator === "x") {
+        return multiply(number1, number2);
+    } else if (operator === "/") {
+        if (number2 === 0) {
+            return "ERROR"; // divide by zero produces an error
+        } else {
+            return divide(number1, number2);
+        }
+    } else {
+        return "ERROR"; // anything else will create an error
+    }
+}
+
+let display = document.getElementById("calcDisplay");
+
+
+
+
